daa6c315bd16c20e80dcf6d3a9547a91aad276d2
SrajalDwivedi <47470035+SrajalDwivedi@users.noreply.github.com>
2021-04-01 11:35:30 +0530

    Add files via upload

---
+++ AddDouble.java
@@ -0,0 +1,9 @@
+public class AddDouble{  
+    public static void main(String args[])  
+    {  
+    int num1=5;  
+    double num2=6.2;  
+    double sum=num1+num2;  
+        System.out.println(sum);  
+     }  
+}  
