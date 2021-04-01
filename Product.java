a7401c1c069f98cd2825cdc25045dd7e4c107699
SrajalDwivedi <47470035+SrajalDwivedi@users.noreply.github.com>
2021-04-01 10:01:36 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,9 @@
+public class Product{  
+    public static void main(String args[])  
+    {  
+    int num1=6;  
+    double num2=8.2;  
+    double product=num1*num2;  
+        System.out.println("Product="+product);  
+     }  
+}  
