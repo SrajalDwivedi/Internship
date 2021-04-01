0546434961e27f1d55f7594e6c0f52e50d042634
SrajalDwivedi <47470035+SrajalDwivedi@users.noreply.github.com>
2021-04-01 12:15:48 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,7 @@
+public class Square {
+    public static void main(String[] args) {
+        double num1=3.9;
+        double square=Math.pow(temp, 2);
+        System.out.println(square);
+    }
+}
