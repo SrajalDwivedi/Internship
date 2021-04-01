b40fdace0d6bb5edf47aa623abbe314f6f001d7f
SrajalDwivedi <47470035+SrajalDwivedi@users.noreply.github.com>
2021-04-01 10:08:06 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle{  
+    public static void main(String args[])  
+    {  
+    int length=3;  
+    int bredth=2;  
+    int area=length*bredth;  
+        System.out.println("Area of rectangle="+area);  
+     }  
+}  
