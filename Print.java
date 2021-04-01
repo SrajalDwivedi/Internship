7febbd8f2f6010f203c855a333d57746b7a9941c
SrajalDwivedi <47470035+SrajalDwivedi@users.noreply.github.com>
2021-04-01 09:16:46 +0530

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,7 @@
+class Print{
+    public static void main(String[] args) {
+        System.out.println("Int "+66);
+        System.out.println("Double "+45.23);
+        System.out.println("Char "+'a');
+    }
+}
