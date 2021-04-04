935dc8a710b0d9b9fb7a7a6b8120421b0d02c520
SrajalDwivedi <47470035+SrajalDwivedi@users.noreply.github.com>
2021-04-04 21:20:23 +0530

    Add files via upload

---
+++ Comparison.java
@@ -0,0 +1,10 @@
+public class Comparison{  
+    public static void main(String args[])  
+    { 
+      System.out.println(compare(23,45));
+
+     } 
+     static String compare(int a,int b){
+        return (a+" is not equal to "+b);
+     }
+}  
