d589eadf883df0e5c3e9f746d0efa4f06f432b80
SrajalDwivedi <47470035+SrajalDwivedi@users.noreply.github.com>
2021-04-04 20:45:29 +0530

    Add files via upload

---
+++ PerimeterTriangle.java
@@ -0,0 +1,10 @@
+public class PerimeterTriangle{  
+    public static void main(String args[])  
+    { 
+      System.out.println("PERIMETER:"+perimeter(2,3,5));
+
+     } 
+     static int perimeter(int a,int b,int c){
+         return a+b+c;
+     }
+}  
